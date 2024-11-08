0e0f6ba50628ba60ad058ad345d420cd60988164
Jorge Cardona <jorgecardona@utp.edu.co>
2024-11-07 19:42:21 -0500

    uodated database path

--- application/configuration/db_path.py
+++ application/configuration/db_path.py
@@ -1,3 +1,4 @@
 # application/configuration/config.py
+# "application/utils/database/music_database_sqlite.db"
 
-DB_PATH = r"application/utils/database/music_database_sqlite.db"
+DB_PATH = r"utils/database/music_database_sqlite.db"
